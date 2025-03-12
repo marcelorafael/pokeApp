9b3cdcead0cc9039d600813422f32741df54e0bf
marcelorafael <marcelorafael23@hotmail.com>
2025-03-11 21:46:35 -0300

    feat: base centerView ok

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,12 +1,24 @@
 import { View, Image } from 'react-native';
 
+import * as S from './styles'
+
 import Pikachu from '../assets/pikachu.png'
 
 const Home = () => {
     return (
-        <View style={{ flex: 1, backgroundColor: '#DD1234' }}>
-            <Image source={Pikachu} style={{width: 300, height: 300}} />
-        </View>
+        // <View style={{ flex: 1, backgroundColor: '#DD1234' }}>
+        //     <Image source={Pikachu} style={{width: 300, height: 300}} />
+        // </View>
+        <S.Wrapper>
+            <S.Header>
+                <Image source={Pikachu} style={{ width: 200, height: 200, marginRight: 50 }} />
+            </S.Header>
+
+            <S.CenterView>
+                <S.CardsView></S.CardsView>
+                <S.SearchView></S.SearchView>
+            </S.CenterView>
+        </S.Wrapper>
     );
 }
 
--- src/screens/styles.ts
+++ src/screens/styles.ts
@@ -1,4 +1,53 @@
-import styled from 'styled-components/native';
+import styled, { css } from 'styled-components/native';
 
-export const StyledText = styled.View`
+export const Wrapper = styled.View`
+  ${({ theme }: any) => css`
+    flex: 1;
+    background-color: ${({ theme }) => theme.colors.primary};
+  `}
 `;
+
+export const Header = styled.View`
+  ${({ theme }: any) => css`
+    /* width: 100%; */
+    height: 30%;
+
+    align-items: center;
+    justify-content: center;
+
+    /* background-color: ${({ theme }) => theme.colors.secondary}; */
+  `}
+`;
+
+export const CenterView = styled.View`
+  ${({ theme }: any) => css`
+    align-items: center;
+  `}
+`;
+
+export const CardsView = styled.View`
+  ${({ theme }: any) => css`
+    width: 90%;
+    height: 300px;
+    background-color: ${({ theme }) => theme.colors.background};
+
+    border-radius: 5px;
+  `}
+`;
+
+export const SearchView = styled.View`
+  ${({ theme }: any) => css`
+    
+  `}
+`;
+
+// export const StyledText = styled.Text`
+//   font-family: ${({ theme }) => theme.fonts.regular};
+//   font-size: 16px;
+//   color: ${({ theme }) => theme.colors.text};
+// `;
+
+// export const StyledView = styled.View`
+//   background-color: ${({ theme }) => theme.colors.background};
+//   padding: ${({ theme }) => theme.spacing.medium}px;
+// `;
